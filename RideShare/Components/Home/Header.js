fc78217db2310db450b4467bc8968a90320eb339
Derek Lam <dereklam67@gmail.com>
2019-02-01 23:11:56 -0800

    added header to home

---
+++ RideShare/Components/Home/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+export  default class Header extends Component {
+  render() {
+    return (
+      <View>
+          <Text style={styles.header}>Ride Share</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 50,
+        color: 'white',
+        textAlign: 'center'
+    }
+});
+
+AppRegistry.registerComponent("Header", () => Header);
